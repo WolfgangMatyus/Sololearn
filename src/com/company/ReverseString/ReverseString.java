9aaadd2df64f17e1f58671ee05c4bd399b3adaa4
unknown <wi21b055@technikum-wien.at>
2022-05-24 23:39:35 +0200

    ReverseString committed

---
+++ src/com/company/ReverseString/ReverseString.java
@@ -0,0 +1,19 @@
+package com.company.ReverseString;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String text = sc.nextLine();
+        char[] arr = text.toCharArray();
+
+        //your code goes here:
+        for(int i = arr.length-1; i >=0; i--){
+            char rev = arr[i];
+            System.out.print(rev);
+        }
+    }
+}
